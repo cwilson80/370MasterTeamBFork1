9973e295922a5b291addbdb28da7d646fe83278c
cwilson80 <cwilson80@murraystate.edu>
2023-09-14 12:12:53 -0500

    Removed unused imports
    Removed a few unused imports, will probably need another pass later.

--- src/CheckerboardGUI.java
+++ src/CheckerboardGUI.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class CheckerboardGUI extends JFrame {
     private static final int BOARD_SIZE = 8;
